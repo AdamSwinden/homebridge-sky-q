11b6b6771f42ba092dd976b44a53548a858edbd9
Adam Swinden <me@swinden.com>
2017-07-26 07:09:00 +0100

    Added Sky Q support.

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # homebridge-sky-q
 
-[Homebridge](https://github.com/nfarina/homebridge) plugin to turn on/off a Sky+ box.
+[Homebridge](https://github.com/nfarina/homebridge) plugin to turn on/off a Sky Q box.
+
+## Installation
+
+Install the plugin globally using npm.
+
+```
+npm install -g homebridge-sky-q
+```
+
+## Configuration
+
+Add this to your `~/.homebridge/config.json` as an accessory:
+```
+{
+	"accessory": "SkyQ",
+	"name": "Sky Q Box",
+	"ipAddress": "<Sky Q Box IP Address>"
+}
+```
+
+## Getting your Sky Q Box's IP address
+
+On your Sky Q Box, go to Settings > Setup > Network > Advanced Settings > IP address.
---
+++ index.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var SkyRemote = require('sky-remote');
+var Accessory, Service, Characteristic;
+
+module.exports = function(homebridge) {
+
+	Accessory = homebridge.platformAccessory;
+	Service = homebridge.hap.Service;
+	Characteristic = homebridge.hap.Characteristic;
+	homebridge.registerAccessory("homebridge-sky-q", "SkyQ", SkyQAccessory);
+};
+
+function SkyQAccessory(log, config, api) {
+
+	this.log = log;
+	this.config = config;
+	this.name = config.name || 'Sky Q';
+
+	var remoteControl = new SkyRemote(config.ipAddress, SkyRemote.SKY_Q);
+	this.skyQ = remoteControl;
+}
+
+
+SkyQAccessory.prototype = {
+
+	setPowerState: function(powerOn, callback) {
+
+		this.log("Sending on command to '" + this.name + "'...");
+
+		this.skyQ.press('power', function(error) {
+
+			if (error) {
+
+				this.log('Failed to turn on ' + this.name + '. ' + error);
+			}
+
+			callback();
+		});
+	},
+
+	identify: function(callback) {
+
+		this.log("Identify...");
+
+		callback();
+	},
+
+	getServices: function() {
+
+		var switchService = new Service.Switch(this.name);
+
+		switchService.getCharacteristic(Characteristic.On).on('set', this.setPowerState.bind(this));
+
+		return [switchService];
+	}
+};
