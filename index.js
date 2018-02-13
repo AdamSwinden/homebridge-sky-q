e306e54f8161b63a51feae4681d71686f75e8799
jdebardi <josedebardi@gmail.com>
2018-02-13 00:58:34 +0000

    Added Auto On feature
    When using the "cmd" option, if the box is off, it will turn it on before sending the cmd. Also works in conjunction with the "delayed" function if enabled.

--- index.js
+++ index.js
@@ -38,32 +38,114 @@ SkyQAccessory.prototype = {
 		var log = this.log;
 		var name = this.name;
 		var stateful = this.stateful;
+		var state = powerOn ? 'on' : 'off';
+		var delayed = this.config.delayed;
+		var skyQ = this.skyQ;
+		var box = this.box;
 		
+		function sendCmd() {
+			log('Sending "' + cmd + '" command to ' + name + '...');
+
+			skyQ.press(cmd, function(error) {
+
+				if (error) {
+
+					log('Failed to send cmd ' + name + '. ' + error);
+				}
+
+				if (stateful === true) {
+	
+					if (state === 'on' && delayed === true) {
+
+						setTimeout(function() {
+
+							skyQ.press('sky', function(error) {
+
+								log('Sending "sky" command to ' + name + '...');
+
+			                	       		if (error) {
+
+                        				        	log('Failed to send cmd ' + name + '. ' + error);
+	                       					}
+
+							});
+
+						}, 20000);
+
+					}
+
+					callback();
+
+				} else {
+
+					callback(new Error(1)); //Only way to keep the switch from turning on/off
+				}
+			});
+		}
 		if (this.config.cmd) {
 
-			this.cmd = this.config.cmd.split(',');
+			var cmd = this.config.cmd.split(',');
 		} else {
 
-			this.cmd = 'power';
+			var cmd = 'power';
 		}
 		
-		log('Sending "' + this.cmd + '" command to ' + name + '...');
+		if (stateful === false && this.config.autoOn === true) {
 
-		this.skyQ.press(this.cmd, function(error) {
+			box.getPowerState().then(isOn=>{
+				if (isOn) {
 
-			if (error) {
+					log(name + " is on :-)")
+					sendCmd();
+				} else {
 
-				log('Failed to send cmd ' + name + '. ' + error);
-			}
+					log(name + " is in standby :-(")
+					skyQ.press(['power','backup'], function(error) {
 
-			if (stateful === true) {	
+						log('Sending "power,backup" command to ' + name + '...');
 
-				callback();
-			} else {
+						if (error) {
 
-				callback(new Error(1)); //Only way to keep the switch from turning on/off
-			}
-		});
+							log('Failed to send cmd ' + name + '. ' + error);
+						}
+					});
+					if (delayed === true) {
+						log("Delayed 'sky' press enabled");
+						setTimeout(function() {
+
+							skyQ.press(['sky','backup'], function(error) {
+
+								log('Sending "sky,backup" command to ' + name + '...');
+								if (error) {
+
+									log('Failed to send cmd ' + name + '. ' + error);
+								}
+							});
+							setTimeout(function() {
+								
+								sendCmd();
+
+							}, 1000); //Give the box a chance to get back to a channel before sending the cmd
+						}, 20000);
+					} else {
+					
+						setTimeout(function() {
+
+							sendCmd();
+
+						}, 3000); //Give the box a chance to start up before we send the cmd
+					}
+				}
+			}).catch(err=>{
+				log("Unable to determine power state")
+				log("Perhaps looking at this error will help you figure out why" + err)
+			})
+
+		} else {
+	
+			sendCmd();
+		}
+
 	},
 
 	identify: function(callback) {
@@ -74,6 +156,7 @@ SkyQAccessory.prototype = {
 	},
 
 	getState: function(callback) {
+		
 		if (this.stateful) {
 
 			this.box.getPowerState().then(isOn=>{
